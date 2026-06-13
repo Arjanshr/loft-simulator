e8e36d25dd46869cb23c4dc6dc07e67b269bebe7
Arjanshr <sirarjan@gmail.com>
2026-06-13 23:00:15 +0545

    Test

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Schedule;
-use Illuminate\Support\Facades\Log;
 
 Schedule::command('pigeons:mature')->hourly();
 Schedule::command('pigeons:market-tick')->hourly();
-
-// Heartbeat: Run every minute to verify Cron is working
-Schedule::call(function () {
-    Log::info('Scheduler Heartbeat: Cron Job is active.');
-})->everyMinute();
